846e34bb5d6d3e0b631154e165316642cc78f88a
saurabhkoshta18 <saurabhkoshta18@gmail.com>
2025-05-28 22:15:20 +0530

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -2,5 +2,10 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
+from flask_migrate import Migrate
+
+migrate = Migrate(app, db)
+
+
 db = SQLAlchemy()
 login_manager = LoginManager()
